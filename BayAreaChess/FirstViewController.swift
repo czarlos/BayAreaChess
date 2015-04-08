68caafcf37b1493f8ff37cf536fb36a909203508
czarlos <cer26@duke.edu>
2015-04-08 14:51:10 -0400

    Initial Commit

---
+++ BayAreaChess/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BayAreaChess
+//
+//  Created by Carlos Reyes on 4/8/15.
+//  Copyright (c) 2015 Carlos Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
