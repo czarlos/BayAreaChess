54626129e2e4d2de9a7cf0fffc8b83233b392f7c
czarlos <cer26@duke.edu>
2015-04-18 00:29:25 -0400

    white text in nav controller

--- BayAreaChess/NavigationController.swift
+++ BayAreaChess/NavigationController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class NavigationController : UINavigationController {
     override func viewDidLoad() {
+        let swiftColor = UIColor(red: 34/255, green: 104/255, blue: 158/255, alpha: 0.5)
+        UINavigationBar.appearance().barTintColor = swiftColor
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
         super.viewDidLoad()
     }
     
