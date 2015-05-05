b288a20cbb2a78f7ec9e36b53b5093fd0fde096c
czarlos <cer26@duke.edu>
2015-05-05 01:09:19 -0400

    app loads tournament data

--- BayAreaChess/TournamentsViewController.swift
+++ BayAreaChess/TournamentsViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 import SwiftHTTP
 
 class TournamentsViewController : UITableViewController {
-    let items = ["foo", "bar", "baz"]
+    var items = ["foo", "bar", "baz"]
+    var eventList = [String]()
+    let SUMMARY = "summary"
+    let GENERAL_TOURNAMENTS_URL = "http://bac.colab.duke.edu:3000/api/v1/tournaments/general"
+    let UI_TABLE_VIEW_CELL = "UITableViewCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getTournaments()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidLoad()
-        getTournaments()
-        self.tableView.reloadData()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -27,25 +30,38 @@ class TournamentsViewController : UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count
+//        return self.items.count
+        return self.eventList.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let item = self.items[indexPath.row]
-        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as! UITableViewCell
-        cell.textLabel!.text = item
+//        let item = self.items[indexPath.row]
+        let item: AnyObject = self.eventList[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier(UI_TABLE_VIEW_CELL, forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel!.text = item as? String
         return cell
     }
     
     func getTournaments() {
         var request = HTTPTask()
-        request.GET("http://bac.colab.duke.edu:3000/api/v1/tournaments/general", parameters: nil, success: {(response: HTTPResponse) in
+        request.GET(GENERAL_TOURNAMENTS_URL, parameters: nil, success: {(response: HTTPResponse) in
             if let data = response.responseObject as? NSData {
-                let str = NSString(data: data, encoding: NSUTF8StringEncoding)
-                println(str) //prints the HTML of the page
+                dispatch_async(dispatch_get_main_queue()) {
+                    let json = JSON(data: data)
+                    self.eventList = self.getEventArray(json)
+                    self.tableView.reloadData()
+                }
             }
             },failure: {(error: NSError, response: HTTPResponse?) in
                 println("error: \(error)")
         })
+    }
+    
+    func getEventArray(json: JSON) -> [String] {
+        var events : [String] = []
+        for item in json.arrayValue {
+            events.append((item[SUMMARY]).string!)
+        }
+        return events
     }
 }
